3b7f3d87be5cd2c1176864fe4a9c0fa6774e3e94
ShrutiAgarwal1988 <105115758+ShrutiAgarwal1988@users.noreply.github.com>
2022-05-19 16:50:23 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class Demo4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i=1;
+		for(i=1;i<=10;i=i+2)
+		{
+		
+			System.out.println(i);
+			
+		}
+		
+		
+		
+		
+	}
+
+}
